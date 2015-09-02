2900f625db605c5b04f3e177323f822811958332
Alejo <Alejo@Lejo>
2015-09-01 22:41:58 -0500

    Probe 2

--- src/cubo/Cubo.java
+++ src/cubo/Cubo.java
@@ -5,12 +5,10 @@ public class Cubo {
 
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
-        double area, volumen, lado;
+        double lado;
         System.out.print("Ingrese el lado del cubo: ");
         lado = lector.nextDouble();
-        area = lado*lado*6;
-        volumen = lado*lado*lado;
-        System.out.println("El area del cubo es: "+area);
-        System.out.println("El volumen del cubo es: "+volumen);     
+        System.out.println("El area del cubo es: "+(lado*lado*6));
+        System.out.println("El volumen del cubo es: "+(lado*lado*lado));     
     }
 }
